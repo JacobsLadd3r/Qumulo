5836e436e3c7b54b905da8a1cb27e5bdf3f50185
Jacob Nicholson <jacob3d@gmail.com>
2020-10-16 04:40:50 -0700

    Calculate capacity as GB not GiB, reduce awk ifs
    - Fix QTEST hostname from q01 to qt01
    - Don't need to sort jq results since the API returns things already by capacity
    - The Qumulo cluster uses KB and MB instead of KiB and MiB so have to /1000 not /1024
    - Don't need to do explicit awk if comparisons to step through B KB MB, use split instead

--- .qqrc.bash
+++ .qqrc.bash
@@ -2,7 +2,7 @@
 export QADMIN=admin
 export QARCH=qa01.storage.example.com
 export QPROD=qp01.storage.example.com
-export QTEST=q01.storage.example.com
+export QTEST=qt01.storage.example.com
 
 # aliases
 alias qplogin='~/Downloads/qumulo_api/qq --host $QPROD login -u $QADMIN'
@@ -16,10 +16,6 @@ alias qt='~/Downloads/qumulo_api/qq --host ${QTEST}'
 qls() {
 qq fs_read_dir_aggregates --path "${1}" | \
 jq -r '.files | to_entries[] | [.value.capacity_usage, .value.num_directories, .value.num_files, .value.num_symlinks, .value.type, .value.name] | @tsv' | \
-sort -nrk1 | \
 awk 'BEGIN{printf "%9s %9s %10s %10s %22s %s\n","capacity","dirs","files","symlinks","type          ","name"}
-{if ($1 > 1099511627776) printf "%6.2f TB %9d %10d %10d %-22s %s\n",$1/1024/1024/1024/1024,$2,$3,$4,$5,substr($0,index($0,$6));
-if ($1 > 1048580000 && $1 < 1099511627776) printf "%6.2f GB %9d %10d %10d %-22s %s\n",$1/1024/1024/1024,$2,$3,$4,$5,substr($0,index($0,$6));
-if ($1 > 1048576 && $1 < 1048579999) printf "%6.2f MB %9d %10d %10d %-22s %s\n",$1/1024/1024,$2,$3,$4,$5,substr($0,index($0,$6));
-if ($1 < 1048576) printf "%6.2f KB %9d %10d %10d %-22s %s\n",$1/1024,$2,$3,$4,$5,substr($0,index($0,$6))}';
+{split("B KB MB GB TB PB",v); s=1; while($1>1000 && s < 6){$1/=1000; s++} printf "%6.2f %2s %9d %10d %10d %-22s %s\n",$1,v[s],$2,$3,$4,$5,substr($0,index($0,$6))}'
 }
