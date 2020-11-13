dbfba849af78a60228c27aea29860a73f24b9041
Jacob Nicholson <jacob3d@gmail.com>
2020-11-13 13:19:14 -0800

    Update awk fields to tabs
    The jq call is already returning tab-delimited data, if awk printed a substr of $6 and that happens to be a digit it would print the entire $0 again and throw off proper printing. Now we just print $6 to include the entire file/folder name, even if it has spaces in it.

--- .qqrc.bash
+++ .qqrc.bash
@@ -16,6 +16,6 @@ alias qt='~/Downloads/qumulo_api/qq --host ${QTEST}'
 qls() {
 qq fs_read_dir_aggregates --path "${1}" | \
 jq -r '.files | to_entries[] | [.value.capacity_usage, .value.num_directories, .value.num_files, .value.num_symlinks, .value.type, .value.name] | @tsv' | \
-awk 'BEGIN{printf "%9s %9s %10s %10s %22s %s\n","capacity","dirs","files","symlinks","type          ","name"}
-{split("B KB MB GB TB PB",v); s=1; while($1>1000 && s < 6){$1/=1000; s++} printf "%6.2f %2s %9d %10d %10d %-22s %s\n",$1,v[s],$2,$3,$4,$5,substr($0,index($0,$6))}'
+awk -F'\t' 'BEGIN{printf "%9s %9s %10s %10s %22s %s\n","capacity","dirs","files","symlinks","type          ","name"}
+{split("B KB MB GB TB PB",v); s=1; while($1>1000 && s < 6){$1/=1000; s++} printf "%6.2f %2s %9d %10d %10d %-22s %s\n",$1,v[s],$2,$3,$4,$5,$6}'
 }
